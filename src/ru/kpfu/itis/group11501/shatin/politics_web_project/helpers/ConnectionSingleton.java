5b5c6b8c830611e58f9b57ec5b5c4c789cb6b221
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2016-10-22 21:07:43 +0300

    Added helpers, connected libraries

---
+++ src/ru/kpfu/itis/group11501/shatin/politics_web_project/helpers/ConnectionSingleton.java
@@ -0,0 +1,29 @@
+package ru.kpfu.itis.group11501.shatin.politics_web_project.helpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Oleg Shatin
+ *         11-501
+ */
+public class ConnectionSingleton {
+    private static Connection connection = null;
+    public static Connection getConnection(){
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ServletApp",
+                        "postgres", "postgres");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        } else return connection;
+    }
+}
